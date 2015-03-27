6a4034d81e1d52c79f338449bddceee6979d3481
David Cruz <dequis@gmail.com>
2015-03-26 23:37:04 -0600

    Adding some comments

--- Pitch Perfect/PlaySoundViewController.swift
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -20,9 +20,11 @@ class PlaySoundViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Initializes the audio player
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
         
+        // Initializes the audio engine
         audioEngine = AVAudioEngine()
         audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
@@ -33,54 +35,67 @@ class PlaySoundViewController: UIViewController {
     }
     
     @IBAction func slowPressed(sender: AnyObject) {
+        // This value changes the speed to be slower
         playWithDiferentRate(0.4)
     }
     
     @IBAction func fastPressed(sender: AnyObject) {
+        // This value change the speed to be faster
         playWithDiferentRate(1.5)
     }
     
     @IBAction func stopPressed(sender: AnyObject) {
-        audioPlayer.stop()
+        // Just stop the audio player and engine
+        stopAudioPlayerAndEngine()
     }
     
     @IBAction func playChipmunkAudio(sender: AnyObject) {
+        // This value changes the pitch to sound like a chipmunk
         playAudioWithVariablePitch(1000)
     }
     
     @IBAction func playDarthVaderAudio(sender: AnyObject) {
+        // This value changes the pitch to sound like darth vader
         playAudioWithVariablePitch(-1050)
     }
     
-    func playWithDiferentRate(rate:Float) {
-        stopAudioPlayerAndEngine()
-        audioPlayer.rate = rate
-        audioPlayer.currentTime = 0.0
-        audioPlayer.play()
-    }
-    
+    // This is the function you were looking for in the previous code review ;)
     func stopAudioPlayerAndEngine() {
+        //Stops the player and engine
         audioPlayer.stop()
         audioEngine.stop()
         audioEngine.reset()
     }
     
+    func playWithDiferentRate(rate:Float) {
+        stopAudioPlayerAndEngine()
+        
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    
     func playAudioWithVariablePitch(pitch: Float) {
         stopAudioPlayerAndEngine()
         
+        // Attaches the audio player
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
         
+        // Attaches the pitch effect
         var changePitchEffect = AVAudioUnitTimePitch()
         changePitchEffect.pitch = pitch
         audioEngine.attachNode(changePitchEffect)
         
+        // Connect the player, the pitch effect and the output
         audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
         audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
         
+        // Charges the audio file
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         audioEngine.startAndReturnError(nil)
 
+        // Play!
         audioPlayerNode.play()
     }
 }
